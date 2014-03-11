7b2b45fe8450caa1d4c099ee10155cd4d4841703
zhangcongle <zhangcongle@gmail.com>
2014-03-11 16:06:41 -0700

    change the htmls so the first line is a json string; choosefile.js will read this line and parse it into a javascript variable " objects\

--- src/js/choosefile.js
+++ src/js/choosefile.js
@@ -1,16 +1,21 @@
+var objects;
+
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
     
     // files is a FileList of File objects. List some properties.
     var output = [];
-    f = files[0];
+    f = files[0];    
     var reader = new FileReader();
     reader.readAsText(f);
     reader.onload = function(e) { 
           var content = e.target.result;
           var display = document.getElementById("display");
           var lines = content.trim().split('\n');
-
+          var json_str = lines[0];
+        json_str = json_str.substring(4,json_str.length-3);
+        objects = eval("(" + json_str + ')');
+        console.log(objects);
         // remove first and last line
         lines.splice(0, 1);
         lines.splice(lines.length - 1, 1);
@@ -19,7 +24,7 @@ function handleFileSelect(evt) {
         content = lines.join('\n');
         content = content.replace(/<label[^>]*>/g, replace);
         content = content.replace(/<\/label>/g, "</span>");
-        console.log(content);
+        // console.log(content);
         display.innerHTML = content;
     }
 }
